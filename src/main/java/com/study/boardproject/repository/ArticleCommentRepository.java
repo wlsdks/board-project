caceb9971aff1e980b1bdfcda3f80b0560375f51
jinan <dig04059@gmail.com>
2023-07-02 01:15:19 +0900

    1. Repository에 @RepositoryRestResource 어노테이션 추가
    data-rest를 사용하기 위해 어노테이션을 repository위에 달아줌

--- src/main/java/com/study/boardproject/repository/ArticleCommentRepository.java
+++ src/main/java/com/study/boardproject/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.study.boardproject.repository;
 
 import com.study.boardproject.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource // spring-data-rest 사용을 위해 작성
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
--- src/main/java/com/study/boardproject/repository/ArticleRepository.java
+++ src/main/java/com/study/boardproject/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.study.boardproject.repository;
 
 import com.study.boardproject.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource // spring-data-rest 사용을 위해 작성
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
