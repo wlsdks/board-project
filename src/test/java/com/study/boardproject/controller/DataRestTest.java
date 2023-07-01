89dc70f9d50fd8ef69e72fab06b5cf5a51b2533b
jinan <dig04059@gmail.com>
2023-07-02 01:38:25 +0900

    1. Spring Data Rest 테스트는 제외처리

--- src/test/java/com/study/boardproject/controller/DataRestTest.java
+++ src/test/java/com/study/boardproject/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.study.boardproject.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 /**
  * data-rest이니 mocking이 힘들어서 그냥 이렇게 직접 db에 접근해서 사용한다.(인테그레이션 테스트 진행)
+ * db에 접근하니 transactional을 적어줘야 자동으로 테스트 후 rollback을 한다.
+ * Mock을 사용하기위해 @AutoConfigureMockMvc 어노테이션을 추가해줘야함
  */
+@Disabled("Spring Data Rest 테스트의 통합 테스트는 불필요함으로 제외시킴")
 @DisplayName("Data Rest - api 테스트")
-@Transactional //db에 접근하니 transactional을 적어줘야 자동으로 테스트 후 rollback을 한다.
-@AutoConfigureMockMvc // Mock을 사용하려면 추가해줘야함
+@Transactional
+@AutoConfigureMockMvc
 @SpringBootTest
 public class DataRestTest {
 
