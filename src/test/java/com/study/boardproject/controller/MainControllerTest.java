f3f8a553cfe943e52c33d0a00ee640d57535c84d
wlsdks <wlsdks12@naver.com>
2023-07-02 22:45:34 +0900

    1. 댓글, 테스트코드 관련 코드 추가

---
+++ src/test/java/com/study/boardproject/controller/MainControllerTest.java
@@ -0,0 +1,34 @@
+package com.study.boardproject.controller;
+
+import com.study.boardproject.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import(SecurityConfig.class)
+@WebMvcTest(MainController.class)
+class MainControllerTest {
+
+    private final MockMvc mvc;
+
+    public MainControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("root 페이지 redirect 테스트")
+    @Test
+    void test1() throws Exception {
+        //given
+
+        //when & then
+        mvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection());
+    }
+
+}
