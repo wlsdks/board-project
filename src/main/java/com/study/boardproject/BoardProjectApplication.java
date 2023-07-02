235acc8a24c41f74dc01d2ac99782a864595081a
jinan <dig04059@gmail.com>
2023-07-02 12:33:22 +0900

    1. thymeleaf 설정 추가

--- src/main/java/com/study/boardproject/BoardProjectApplication.java
+++ src/main/java/com/study/boardproject/BoardProjectApplication.java
@@ -2,7 +2,9 @@ package com.study.boardproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan //thymeleaf conf 오류해결
 @SpringBootApplication
 public class BoardProjectApplication {
 
