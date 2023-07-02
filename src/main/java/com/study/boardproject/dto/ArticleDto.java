9e5b66e69a3f031db794f5b991ee68003d320cce
wlsdks <wlsdks12@naver.com>
2023-07-03 01:39:31 +0900

    1. 코드주석 추가

--- src/main/java/com/study/boardproject/dto/ArticleDto.java
+++ src/main/java/com/study/boardproject/dto/ArticleDto.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 /**
  * DTO for {@link com.study.boardproject.domain.Article}
+ * 이렇게 설계하면 entity는 dto를 의존하지 않아서 영향을 받지 않는다.
  */
 public record ArticleDto(
         Long id,
@@ -22,6 +23,7 @@ public record ArticleDto(
         return new ArticleDto(id, userAccountDto, title, content, hashtag, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
+    // entity를 dto로 변환해주는 코드
     public static ArticleDto from(Article entity) {
         return new ArticleDto(
                 entity.getId(),
@@ -36,6 +38,7 @@ public record ArticleDto(
         );
     }
 
+    // dto를 entity로 만들어주는 코드
     public Article toEntity() {
         return Article.of(
                 userAccountDto.toEntity(),
