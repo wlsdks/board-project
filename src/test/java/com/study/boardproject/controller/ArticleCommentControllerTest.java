3a3921e3b5611371f3b2f5e1d98563bf9f35479c
wlsdks <wlsdks12@naver.com>
2023-07-05 10:35:41 +0900

    1. articleComment 컨트롤러 생성 및 tdd 테스트코드 작성

---
+++ src/test/java/com/study/boardproject/controller/ArticleCommentControllerTest.java
@@ -0,0 +1,93 @@
+package com.study.boardproject.controller;
+
+import com.study.boardproject.config.SecurityConfig;
+import com.study.boardproject.dto.ArticleCommentDto;
+import com.study.boardproject.dto.request.ArticleCommentRequest;
+import com.study.boardproject.service.ArticleCommentService;
+import com.study.boardproject.util.FormDataEncoder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@Import({SecurityConfig.class, FormDataEncoder.class})
+@WebMvcTest(ArticleCommentController.class) // test하려는 클래스만 따로 넣는다.
+class ArticleCommentControllerTest {
+
+    private final MockMvc mockMvc;
+    private final FormDataEncoder formDataEncoder;
+
+    @MockBean
+    private ArticleCommentService articleCommentService;
+
+    @Autowired
+    public ArticleCommentControllerTest(MockMvc mockMvc, FormDataEncoder formDataEncoder) {
+        this.mockMvc = mockMvc;
+        this.formDataEncoder = formDataEncoder;
+    }
+
+    @DisplayName("[view] [Post] 댓글 등록 - 정상 호출")
+    @Test
+    void commentInsertTest() throws Exception {
+        //given
+        long articleId = 1L;
+        ArticleCommentRequest request = ArticleCommentRequest.of(articleId, "test comment");
+        willDoNothing().given(articleCommentService).saveArticleComment(any(ArticleCommentDto.class));
+
+        //when
+        mockMvc.perform(post("/comments/new")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content(formDataEncoder.encode(request))
+                        .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles/" + articleId))
+                .andExpect(redirectedUrl("/articles/" + articleId));
+
+        //then
+        then(articleCommentService).should().saveArticleComment(any(ArticleCommentDto.class));
+    }
+
+    @DisplayName("[view] [Post] 댓글 삭제 - 정상 호출")
+    @Test
+    void commentDeleteTest() throws Exception {
+        //given
+        long articleId = 1L;
+        long articleCommentId = 1L;
+        willDoNothing().given(articleCommentService).deleteArticleComment(articleCommentId);
+
+        //when
+        mockMvc.perform(post("/comments/" + articleCommentId + "/delete")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content(formDataEncoder.encode(Map.of("articleId", articleId)))
+                        .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles/" + articleId))
+                .andExpect(redirectedUrl("/articles/" + articleId));
+
+        //then
+        then(articleCommentService).should().deleteArticleComment(articleCommentId);
+    }
+
+
+
+
+
+
+
+}
