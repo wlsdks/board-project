e46fc62290e6d53c74a979a8faefed124cd7f7aa
wlsdks <wlsdks12@naver.com>
2023-07-05 20:27:26 +0900

    1. 수정, 삭제를 할 때 해당 사용자가 맞는지 확인하기 위한 응답 정보 추가

--- src/main/java/com/study/boardproject/dto/response/ArticleCommentResponse.java
+++ src/main/java/com/study/boardproject/dto/response/ArticleCommentResponse.java
@@ -2,7 +2,6 @@ package com.study.boardproject.dto.response;
 
 import com.study.boardproject.dto.ArticleCommentDto;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
 public record ArticleCommentResponse(
@@ -10,11 +9,12 @@ public record ArticleCommentResponse(
         String content,
         LocalDateTime createdAt,
         String email,
-        String nickname
-) implements Serializable {
+        String nickname,
+        String userId
+) {
 
-    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname) {
-        return new ArticleCommentResponse(id, content, createdAt, email, nickname);
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId) {
+        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId);
     }
 
     public static ArticleCommentResponse from(ArticleCommentDto dto) {
@@ -28,7 +28,8 @@ public record ArticleCommentResponse(
                 dto.content(),
                 dto.createdAt(),
                 dto.userAccountDto().email(),
-                nickname
+                nickname,
+                dto.userAccountDto().userId()
         );
     }
 
