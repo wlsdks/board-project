06d18e4dc324954c7ec6cdada81ddc5d167f9b37
wlsdks12 <wlsdks12@naver.com>
2023-07-07 15:19:01 +0900

    1. 임시커밋

--- src/main/java/com/study/boardproject/dto/request/ArticleCommentRequest.java
+++ src/main/java/com/study/boardproject/dto/request/ArticleCommentRequest.java
@@ -6,16 +6,25 @@ import com.study.boardproject.dto.UserAccountDto;
 /**
  * DTO for {@link com.study.boardproject.domain.ArticleComment}
  */
-public record ArticleCommentRequest(Long articleId, String content) {
+public record ArticleCommentRequest(
+        Long articleId,
+        Long parentCommentId, // check
+        String content
+) {
 
     public static ArticleCommentRequest of(Long articleId, String content) {
-        return new ArticleCommentRequest(articleId, content);
+        return ArticleCommentRequest.of(articleId, null, content);
+    }
+
+    public static ArticleCommentRequest of(Long articleId, Long parentCommentId, String content) {
+        return new ArticleCommentRequest(articleId, parentCommentId, content);
     }
 
     public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
         return ArticleCommentDto.of(
                 articleId,
                 userAccountDto,
+                parentCommentId,
                 content
         );
     }
--- src/main/java/com/study/boardproject/dto/response/ArticleCommentResponse.java
+++ src/main/java/com/study/boardproject/dto/response/ArticleCommentResponse.java
@@ -3,6 +3,9 @@ package com.study.boardproject.dto.response;
 import com.study.boardproject.dto.ArticleCommentDto;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public record ArticleCommentResponse(
         Long id,
@@ -10,11 +13,22 @@ public record ArticleCommentResponse(
         LocalDateTime createdAt,
         String email,
         String nickname,
-        String userId
+        String userId,
+        Long parentCommentId, // check
+        Set<ArticleCommentResponse> childComments
 ) {
 
     public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId) {
-        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId);
+        return ArticleCommentResponse.of(id, content, createdAt, email, nickname, userId, null);
+    }
+
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId, Long parentCommentId) {
+        // 정렬된 대댓글을 내보내기 위해 TreeSet을 사용한다.
+        Comparator<ArticleCommentResponse> childCommentComparator = Comparator
+                .comparing(ArticleCommentResponse::createdAt)
+                .thenComparing(ArticleCommentResponse::id);
+
+        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId, parentCommentId, new TreeSet<>(childCommentComparator));
     }
 
     public static ArticleCommentResponse from(ArticleCommentDto dto) {
@@ -23,14 +37,21 @@ public record ArticleCommentResponse(
             nickname = dto.userAccountDto().userId();
         }
 
-        return new ArticleCommentResponse(
+        return ArticleCommentResponse.of(
                 dto.id(),
                 dto.content(),
                 dto.createdAt(),
                 dto.userAccountDto().email(),
                 nickname,
-                dto.userAccountDto().userId()
+                dto.userAccountDto().userId(),
+                dto.parentCommentId()
         );
+    }
+
+    // 부모인지 자식인지 판단하는 메서드
+    public boolean hasParentComment() {
+        // 내가 parentCommentId를 가졌는지 아닌지를 검사하면 된다.
+        return parentCommentId != null;
     }
 
 }
