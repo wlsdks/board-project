9ca5e56ad3ab8971ee9ab6c331615ce318a0fb0f
wlsdks <wlsdks12@naver.com>
2023-07-16 16:22:07 +0900

    1. UserAccount 엔티티의 펙토리 메소드 변환

--- src/main/java/com/study/boardproject/dto/ArticleDto.java
+++ src/main/java/com/study/boardproject/dto/ArticleDto.java
@@ -23,14 +23,20 @@ public record ArticleDto(
         String modifiedBy
 ) {
 
+
+    // request에서 사용될 ArticleDto를 만들때 사용되는 factory 메소드
     public static ArticleDto of(UserAccountDto userAccountDto, String title, String content, Set<HashtagDto> hashtagDtos) {
         return new ArticleDto(null, userAccountDto, title, content, hashtagDtos, null, null, null, null);
     }
 
+    // 테스트 코드에서 사용되는 factory 메소드
     public static ArticleDto of(Long id, UserAccountDto userAccountDto, String title, String content, Set<HashtagDto> hashtagDtos, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
         return new ArticleDto(id, userAccountDto, title, content, hashtagDtos, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
+    /**
+     * 실제 서비스 레이어에서 사용되는 엔티티를 dto로 변환시켜주는 팩토리 메소드
+     */
     public static ArticleDto from(Article entity) {
         return new ArticleDto(
                 entity.getId(),
