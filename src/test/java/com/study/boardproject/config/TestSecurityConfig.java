4e6bc5a7ec33a83197d4c6832003e06027a71ab0
wlsdks <wlsdks12@naver.com>
2023-07-16 16:24:38 +0900

    1. 기존 security config에서 repository를 사용하던것을 service레이어로 변경 후 모든 테스트에 생긴 오류를 해결하기위해 test전용 시큐리티 설정코드 수정

--- src/test/java/com/study/boardproject/config/TestSecurityConfig.java
+++ src/test/java/com/study/boardproject/config/TestSecurityConfig.java
@@ -1,7 +1,7 @@
 package com.study.boardproject.config;
 
-import com.study.boardproject.domain.UserAccount;
-import com.study.boardproject.repository.UserAccountRepository;
+import com.study.boardproject.dto.UserAccountDto;
+import com.study.boardproject.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
@@ -17,17 +17,25 @@ import static org.mockito.BDDMockito.given;
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
 
-    @MockBean private UserAccountRepository userAccountRepository;
+    @MockBean private UserAccountService userAccountService;
 
     @BeforeTestMethod
     public void securitySetUp() {
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
                 "unoTest",
                 "pw",
                 "uno-test@email.com",
                 "uno-test",
                 "test memo"
-        )));
+        );
     }
 
 }
